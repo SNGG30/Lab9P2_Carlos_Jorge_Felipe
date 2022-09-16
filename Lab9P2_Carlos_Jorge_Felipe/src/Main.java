f14160b6f8be1e60e30fdaab7229e3c8eb259d69
Felipe Garcia <Felipex.Garcia11@Gmail.com>
2022-09-16 14:02:28 -0600

    Version 2

--- Lab9P2_Carlos_Jorge_Felipe/src/Main.java
+++ Lab9P2_Carlos_Jorge_Felipe/src/Main.java
@@ -26,36 +26,57 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        BG_Login = new javax.swing.JPanel();
+        txt_login = new javax.swing.JLabel();
+        txt_user = new javax.swing.JLabel();
+        tf_User = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Login");
+        setResizable(false);
 
-        jPanel1.setBackground(new java.awt.Color(255, 51, 51));
+        txt_login.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        txt_login.setText("LOGIN");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 370, Short.MAX_VALUE)
+        txt_user.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txt_user.setText("Usuario");
+
+        javax.swing.GroupLayout BG_LoginLayout = new javax.swing.GroupLayout(BG_Login);
+        BG_Login.setLayout(BG_LoginLayout);
+        BG_LoginLayout.setHorizontalGroup(
+            BG_LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BG_LoginLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(txt_login)
+                .addGap(265, 265, 265))
+            .addGroup(BG_LoginLayout.createSequentialGroup()
+                .addGap(67, 67, 67)
+                .addGroup(BG_LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txt_user)
+                    .addComponent(tf_User, javax.swing.GroupLayout.PREFERRED_SIZE, 523, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(65, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 262, Short.MAX_VALUE)
+        BG_LoginLayout.setVerticalGroup(
+            BG_LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(BG_LoginLayout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(txt_login)
+                .addGap(111, 111, 111)
+                .addComponent(txt_user)
+                .addGap(27, 27, 27)
+                .addComponent(tf_User, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(271, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 30, Short.MAX_VALUE))
+            .addComponent(BG_Login, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 38, Short.MAX_VALUE))
+            .addComponent(BG_Login, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -97,6 +118,9 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel BG_Login;
+    private javax.swing.JTextField tf_User;
+    private javax.swing.JLabel txt_login;
+    private javax.swing.JLabel txt_user;
     // End of variables declaration//GEN-END:variables
 }
