83bdc835b4f9ff52ed8a4b4addb0ec9ef5d16e92
Felipe Garcia <Felipex.Garcia11@Gmail.com>
2022-09-16 14:38:46 -0600

    Version 4

--- Lab9P2_Carlos_Jorge_Felipe/src/Main.java
+++ Lab9P2_Carlos_Jorge_Felipe/src/Main.java
@@ -26,12 +26,71 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Registro = new javax.swing.JFrame();
+        txt_Registro = new javax.swing.JLabel();
+        btn_Register = new javax.swing.JButton();
+        btn_Back = new javax.swing.JButton();
         BG_Login = new javax.swing.JPanel();
         txt_login = new javax.swing.JLabel();
         txt_user = new javax.swing.JLabel();
         tf_User = new javax.swing.JTextField();
         txt_pass = new javax.swing.JLabel();
         pf_pass = new javax.swing.JPasswordField();
+        btn_sign = new javax.swing.JToggleButton();
+        btn_reg = new javax.swing.JToggleButton();
+
+        Registro.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        Registro.setPreferredSize(new java.awt.Dimension(655, 539));
+        Registro.setResizable(false);
+
+        txt_Registro.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        txt_Registro.setText("REGISTRO");
+
+        btn_Register.setText("Registrar");
+        btn_Register.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_RegisterActionPerformed(evt);
+            }
+        });
+
+        btn_Back.setText("Regresar");
+        btn_Back.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_BackMouseClicked(evt);
+            }
+        });
+        btn_Back.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_BackActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout RegistroLayout = new javax.swing.GroupLayout(Registro.getContentPane());
+        Registro.getContentPane().setLayout(RegistroLayout);
+        RegistroLayout.setHorizontalGroup(
+            RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(RegistroLayout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(btn_Back, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(67, 67, 67)
+                .addComponent(txt_Registro)
+                .addContainerGap(250, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, RegistroLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btn_Register, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(63, 63, 63))
+        );
+        RegistroLayout.setVerticalGroup(
+            RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(RegistroLayout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addGroup(RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txt_Registro)
+                    .addComponent(btn_Back, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 362, Short.MAX_VALUE)
+                .addComponent(btn_Register, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(42, 42, 42))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
@@ -46,12 +105,31 @@ public class Main extends javax.swing.JFrame {
         txt_pass.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         txt_pass.setText("Contraseña");
 
+        btn_sign.setText("Ingresar");
+        btn_sign.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_signActionPerformed(evt);
+            }
+        });
+
+        btn_reg.setText("Registro");
+        btn_reg.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_regMouseClicked(evt);
+            }
+        });
+        btn_reg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_regActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout BG_LoginLayout = new javax.swing.GroupLayout(BG_Login);
         BG_Login.setLayout(BG_LoginLayout);
         BG_LoginLayout.setHorizontalGroup(
             BG_LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BG_LoginLayout.createSequentialGroup()
-                .addContainerGap(279, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(txt_login)
                 .addGap(265, 265, 265))
             .addGroup(BG_LoginLayout.createSequentialGroup()
@@ -62,6 +140,12 @@ public class Main extends javax.swing.JFrame {
                     .addComponent(tf_User)
                     .addComponent(pf_pass, javax.swing.GroupLayout.DEFAULT_SIZE, 523, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(BG_LoginLayout.createSequentialGroup()
+                .addGap(82, 82, 82)
+                .addComponent(btn_sign, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 128, Short.MAX_VALUE)
+                .addComponent(btn_reg, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(119, 119, 119))
         );
         BG_LoginLayout.setVerticalGroup(
             BG_LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -76,7 +160,11 @@ public class Main extends javax.swing.JFrame {
                 .addComponent(txt_pass)
                 .addGap(28, 28, 28)
                 .addComponent(pf_pass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(140, Short.MAX_VALUE))
+                .addGap(54, 54, 54)
+                .addGroup(BG_LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_sign, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn_reg, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(30, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -92,6 +180,46 @@ public class Main extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_signActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_signActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_signActionPerformed
+
+    private void btn_regActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_regActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_regActionPerformed
+
+    private void btn_regMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_regMouseClicked
+        // TODO add your handling code here:
+        
+        Registro.pack();
+        Registro.setLocationRelativeTo(this);
+        Registro.setAlwaysOnTop(true);
+        Registro.setVisible(true);
+        
+        this.setVisible(false);
+        this.setEnabled(false);
+    }//GEN-LAST:event_btn_regMouseClicked
+
+    private void btn_RegisterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_RegisterActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_RegisterActionPerformed
+
+    private void btn_BackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_BackActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_BackActionPerformed
+
+    private void btn_BackMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_BackMouseClicked
+        // TODO add your handling code here:
+       
+        this.pack();
+        this.setLocationRelativeTo(Registro);
+        this.setAlwaysOnTop(true);
+        this.setVisible(true);
+        
+        Registro.setVisible(false);
+        Registro.setEnabled(false);
+    }//GEN-LAST:event_btn_BackMouseClicked
 
     /**
      * @param args the command line arguments
@@ -130,8 +258,14 @@ public class Main extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BG_Login;
+    private javax.swing.JFrame Registro;
+    private javax.swing.JButton btn_Back;
+    private javax.swing.JButton btn_Register;
+    private javax.swing.JToggleButton btn_reg;
+    private javax.swing.JToggleButton btn_sign;
     private javax.swing.JPasswordField pf_pass;
     private javax.swing.JTextField tf_User;
+    private javax.swing.JLabel txt_Registro;
     private javax.swing.JLabel txt_login;
     private javax.swing.JLabel txt_pass;
     private javax.swing.JLabel txt_user;
